573e965d62b45e5c8f2627d38fe07d882a314409
iammatthias <me@iammatthias.com>
2022-07-07 08:46:54 -0700

    ugh

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,111 +1,96 @@
+import { readFileSync } from "fs";
+
 import { sanitizeHtml } from "./sanitizer";
 import { ParsedRequest } from "./types";
 
+const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString("base64");
+const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString("base64");
+const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString("base64");
+
 function getCss(theme: string, fontSize: string) {
-  let background = "#faf9f9";
-  let foreground = "#0c0c0e";
+  let background = "white";
+  let foreground = "black";
 
   if (theme === "dark") {
-    background = "#0c0c0e";
-    foreground = "#faf9f9";
+    background = "black";
+    foreground = "white";
   }
   return `
+    @font-face {
+        font-family: 'Inter';
+        font-style:  normal;
+        font-weight: normal;
+        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Inter';
+        font-style:  normal;
+        font-weight: bold;
+        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Vera';
+        font-style: normal;
+        font-weight: normal;
+        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
+      }
 
     body {
         background: ${background};
         background-image: radial-gradient(circle at 50% 50%, rgba(249, 254, 255, 1), rgba(253, 255, 252, .2)), url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='6' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%' height='100%' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
         background-size: cover;
-        height: 100vh;
+        height: calc(100vh - 256px);
         display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
-    }
-
-    code {
-        color: #D400FF;
-        font-family: 'Space Mono';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
-    }
-
-    code:before, code:after {
-        content: '\`';
-    }
-
-    .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
-    }
-
-    .logo {
-        margin: 0 75px;
-    }
-
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
-    }
-
-    .spacer {
-        margin: 150px;
-    }
-
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 128px;
     }
     
     .heading {
         font-family: 'Inter', sans-serif;
         font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal; 
+        font-style: normal;
         color: ${foreground};
-        line-height: 1.4;
+        line-height: 1.8;
+    }
+
+    .subHeading {
+        font-family: 'Inter', sans-serif;
+        font-size: 32px;
+        font-style: normal;
+        font-weight: bold;
+        color: ${foreground};
+        line-height: 1.8;
     }`;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-  const { text, theme, fontSize, images, widths, heights } = parsedReq;
+  const { text, theme, fontSize } = parsedReq;
   return `<!DOCTYPE html>
-    <html>
+<html>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Space+Mono&display=swap" rel="stylesheet">
     <style>
         ${getCss(theme, fontSize)}
     </style>
     <body>
+            
+        <img
+            class="logo"
+            alt="Generated Image"
+            src="http://iammatthias.com/images/safari-pinned-tab.svg"
+            width="192"
+            height="192"
+        />
         <div>
-            <div class="spacer">
-            <div class="logo-wrapper">
-                ${images.map((img, i) => getPlusSign(i) + getImage(img, widths[i], heights[i])).join("")}
-            </div>
-            <div class="spacer">
-            <div class="heading">${sanitizeHtml(text)}
-            </div>
+            <div class="heading">${sanitizeHtml(text)}</div>
+            <hr/>
+            <div class="subHeading">iammatthias.com</div>
         </div>
+      
     </body>
 </html>`;
-}
-
-function getImage(src: string, width = "auto", height = "225") {
-  return `<img
-        class="logo"
-        alt="Generated Image"
-        src="${sanitizeHtml(src)}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
-    />`;
-}
-
-function getPlusSign(i: number) {
-  return i === 0 ? "" : '<div class="plus">+</div>';
 }
