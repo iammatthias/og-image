741136e0fc5878039f1006295c74907f73d5ce61
iammatthias <me@iammatthias.com>
2022-07-06 17:39:39 -0700

    ugh

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -1,63 +1,63 @@
-import { IncomingMessage } from "http";
-import { parse } from "url";
-import { ParsedRequest, Theme } from "./types";
+import { IncomingMessage } from 'http';
+import { parse } from 'url';
+import { ParsedRequest, Theme } from './types';
 
 export function parseRequest(req: IncomingMessage) {
-  console.log("HTTP " + req.url);
-  const { pathname, query } = parse(req.url || "/", true);
-  const { fontSize, images, widths, heights, theme, md } = query || {};
+    console.log('HTTP ' + req.url);
+    const { pathname, query } = parse(req.url || '/', true);
+    const { fontSize, images, widths, heights, theme, md } = (query || {});
 
-  if (Array.isArray(fontSize)) {
-    throw new Error("Expected a single fontSize");
-  }
-  if (Array.isArray(theme)) {
-    throw new Error("Expected a single theme");
-  }
+    if (Array.isArray(fontSize)) {
+        throw new Error('Expected a single fontSize');
+    }
+    if (Array.isArray(theme)) {
+        throw new Error('Expected a single theme');
+    }
+    
+    const arr = (pathname || '/').slice(1).split('.');
+    let extension = '';
+    let text = '';
+    if (arr.length === 0) {
+        text = '';
+    } else if (arr.length === 1) {
+        text = arr[0];
+    } else {
+        extension = arr.pop() as string;
+        text = arr.join('.');
+    }
 
-  const arr = (pathname || "/").slice(1).split(".");
-  let extension = "";
-  let text = "";
-  if (arr.length === 0) {
-    text = "";
-  } else if (arr.length === 1) {
-    text = arr[0];
-  } else {
-    extension = arr.pop() as string;
-    text = arr.join(".");
-  }
-
-  const parsedRequest: ParsedRequest = {
-    fileType: extension === "jpeg" ? extension : "png",
-    text: decodeURIComponent(text),
-    theme: theme === "dark" ? "dark" : "light",
-    md: md === "1" || md === "true",
-    fontSize: fontSize || "96px",
-    images: getArray(images),
-    widths: getArray(widths),
-    heights: getArray(heights),
-  };
-  parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
-  return parsedRequest;
+    const parsedRequest: ParsedRequest = {
+        fileType: extension === 'jpeg' ? extension : 'png',
+        text: decodeURIComponent(text),
+        theme: theme === 'dark' ? 'dark' : 'light',
+        md: md === '1' || md === 'true',
+        fontSize: fontSize || '96px',
+        images: getArray(images),
+        widths: getArray(widths),
+        heights: getArray(heights),
+    };
+    parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
+    return parsedRequest;
 }
 
 function getArray(stringOrArray: string[] | string | undefined): string[] {
-  if (typeof stringOrArray === "undefined") {
-    return [];
-  } else if (Array.isArray(stringOrArray)) {
-    return stringOrArray;
-  } else {
-    return [stringOrArray];
-  }
+    if (typeof stringOrArray === 'undefined') {
+        return [];
+    } else if (Array.isArray(stringOrArray)) {
+        return stringOrArray;
+    } else {
+        return [stringOrArray];
+    }
 }
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
-  const defaultImage = theme === "light" ? "http://iammatthias.com/images/safari-pinned-tab.svg" : "http://iammatthias.com/images/safari-pinned-tab.svg";
+    const defaultImage = theme === 'light' ? 'https://og.iammatthias.com/img/logo-dark.png' : 'https://og.iammatthias.com/img/logo-light.png';
 
-  if (!images || !images[0]) {
-    return [defaultImage];
-  }
-  if (!images[0].startsWith("https://assets.vercel.com/") && !images[0].startsWith("https://assets.zeit.co/")) {
-    images[0] = defaultImage;
-  }
-  return images;
+    if (!images || !images[0]) {
+        return [defaultImage];
+    }
+    if (!images[0].startsWith('https://assets.vercel.com/') && !images[0].startsWith('https://assets.zeit.co/')) {
+        images[0] = defaultImage;
+    }
+    return images;
 }
